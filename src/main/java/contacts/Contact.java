91373d9abfb4953ed9ce7ae5c873fd7d56ea0ae8
dongxiaoping1015 <xiaoping_1015@163.com>
2018-12-20 08:03:04 +0800

    all

---
+++ src/main/java/contacts/Contact.java
@@ -0,0 +1,49 @@
+package contacts;
+
+public class Contact {
+    private Long Id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
